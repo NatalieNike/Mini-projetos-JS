c8a8e3682f6f06c379da07f1cbb5781f52609cba
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-01-09 21:43:56 -0300

    Adicionando desafio da lâmpada

---
+++ lampada/lampada.js
@@ -0,0 +1,9 @@
+function turnOn() {
+   let on = document.getElementById("lamp")
+   on.src = 'imgs/ligada.jpg'
+}
+
+function turnOff() {
+    let off = document.getElementById("lamp")
+    off.src = 'imgs/desligada.jpg'
+}
