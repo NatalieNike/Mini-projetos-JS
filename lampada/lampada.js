7ed82e5cab712fc50aebba3e7fd512759295f6c8
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-01-20 20:04:41 -0300

    Add files via upload

--- lampada/lampada.js
+++ lampada/lampada.js
@@ -1,9 +1,39 @@
+const turnOnOff = document.getElementById('turnOnOff')
+const lamp = document.getElementById('lamp')
+
+
+function isLampBroken() { 
+   return lamp.src.indexOf('quebrada') > -1
+}
+
 function turnOn() {
-   let on = document.getElementById("lamp")
-   on.src = 'imgs/ligada.jpg'
+    if (!isLampBroken()) {
+    lamp.src = 'imgs/ligada.jpg'
+    }
 }
 
 function turnOff() {
-    let off = document.getElementById("lamp")
-    off.src = 'imgs/desligada.jpg'
+    if (!isLampBroken()) {
+    lamp.src = 'imgs/desligada.jpg'
+    }
 }
+
+function lampBroken() {
+    lamp.src = 'imgs/quebrada.jpg'
+}
+
+function lampOnOff() {
+    if (turnOnOff.textContent == 'Ligar') {
+        turnOn();
+        turnOnOff.textContent = 'Desligar'
+    } else {
+        turnOff();
+        turnOnOff.textContent = 'Ligar'
+    }
+}
+
+turnOnOff.addEventListener('click', lampOnOff)
+
+lamp.addEventListener('mouseover', turnOn)
+lamp.addEventListener('mouseleave', turnOff)
+lamp.addEventListener('dblclick', lampBroken)
