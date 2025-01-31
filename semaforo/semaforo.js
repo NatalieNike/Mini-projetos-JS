d406053f052a006c043c3c9c1716c7d59693eddc
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-01-31 00:02:41 -0300

    Add files via upload

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -1,9 +1,22 @@
-const estado_semaforo = document.getElementById('imgSemaforo')
+const img = document.getElementById('imgSemaforo')
 const buttons = document.getElementById('buttons-section')
 
+const trafficLight = (event) => {
+    let cor = event.target.id 
+    if (cor == 'red') {
+        turnOn.red()
+    } else if (cor == 'yellow') {
+        turnOn.yellow()
+    } else if (cor == 'green') {
+        turnOn.green()
+    }
+}
 
 const turnOn = {
-    'red': () => img.src = 'imgs/vermelho.png' 
+    'red': () => img.src = 'imgs/vermelho.png',
+    'yellow': () => img.src = 'imgs/amarelo.png',
+    'green': () => img.src = 'imgs/verde.png'
 }
+
 
 buttons.addEventListener('click', trafficLight)
