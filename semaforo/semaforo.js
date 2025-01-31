582e8ef75518f243d57b11403d8ebc6322a5da7c
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-01-31 00:00:35 -0300

    Add files via upload

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -1,21 +1,9 @@
-const red = document.getElementById('red') 
-const yellow = document.getElementById('yellow')
-const green = document.getElementById('green')
-
 const estado_semaforo = document.getElementById('imgSemaforo')
+const buttons = document.getElementById('buttons-section')
 
-function turnRed() {
-    estado_semaforo.src = 'imgs/vermelho.png'
+
+const turnOn = {
+    'red': () => img.src = 'imgs/vermelho.png' 
 }
 
-function turnYellow() {
-    estado_semaforo.src = 'imgs/amarelo.png'
-}
-
-function turnGreen() {
-    estado_semaforo.src = 'imgs/verde.png'
-}
-
-red.addEventListener('click', turnRed)
-yellow.addEventListener('click', turnYellow)
-green.addEventListener('click', turnGreen)
+buttons.addEventListener('click', trafficLight)
